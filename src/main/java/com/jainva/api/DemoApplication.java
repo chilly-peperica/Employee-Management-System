1832eec2418ca3db38ce3c9a0174cef3cd186aea
Vaibhav Jain <jainva@vmware.com>
2023-04-18 01:09:24 +0530

    Commit after name change - not working

--- src/main/java/com/jainva/api/DemoApplication.java
+++ src/main/java/com/jainva/api/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.jainva.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@OpenAPIDefinition(info = @Info(title = "Employee Management server"))
 public class DemoApplication {
 
 	public static void main(String[] args) {

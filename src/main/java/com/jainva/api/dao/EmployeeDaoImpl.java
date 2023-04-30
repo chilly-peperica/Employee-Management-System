7f423d5849069fed04dd35da5799111a364d4bb7
Vaibhav Jain <jainva@vmware.com>
2023-05-01 02:12:57 +0530

    Get all employees API, lombork integeration and mapping directly using resultSet

--- src/main/java/com/jainva/api/dao/EmployeeDaoImpl.java
+++ src/main/java/com/jainva/api/dao/EmployeeDaoImpl.java
@@ -1,18 +1,18 @@
 package com.jainva.api.dao;
 
 import com.jainva.api.utils.DateUtils;
-import com.openapi.gen.springboot.dto.CorporateDetails;
-import com.openapi.gen.springboot.dto.CreateEmployeeRequest;
-import com.openapi.gen.springboot.dto.Employee;
-import com.openapi.gen.springboot.dto.PersonalDetails;
+import com.openapi.gen.springboot.dto.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Component
+@Slf4j
 public class EmployeeDaoImpl implements EmployeeDao {
+
 
 
     public final JdbcTemplate jdbcTemplate;
@@ -22,7 +22,39 @@ public class EmployeeDaoImpl implements EmployeeDao {
     }
     @Override
     public List<Employee> getAllEmployees(){
-        return new ArrayList<>();
+        try {
+            log.info("Retrieving data from DB");
+            return jdbcTemplate.query(DataBaseQueries.GET_ALL_EMPLOYEES, (resultSet, i) -> {
+                Employee e = new Employee();
+
+                PersonalDetails p = new PersonalDetails();
+                p.setName(resultSet.getString("name"));
+                Address a = new Address();
+                a.setCountry(resultSet.getString("country"));
+                a.setPinCode(resultSet.getLong("pin_code"));
+                a.setCity(resultSet.getString("city"));
+                a.setState(resultSet.getString("state"));
+
+                p.setAddress(a);
+                p.setMobileNumber(resultSet.getString("mobile_number"));
+
+                e.setPersonalDetails(p);
+
+                CorporateDetails c = new CorporateDetails();
+                c.setSalary(resultSet.getInt("salary"));
+                Date date = resultSet.getDate("joining_date");
+                c.setJoiningDate(
+                        date == null ? null : DateUtils.asLocalDate(date) );
+                c.setPositionId(resultSet.getInt("position_id"));
+
+                e.setCorporateDetails(c);
+
+                return e;
+            });
+        }catch (Exception e){
+            log.error("Failed to map data from database with message : {} with trace : {}", e.getMessage(), e.getStackTrace());
+        }
+        return null;
     }
 
     @Override
@@ -35,7 +67,7 @@ public class EmployeeDaoImpl implements EmployeeDao {
                     pd.getName(), pd.getAddress().getCountry(),
                     pd.getAddress().getPinCode(), pd.getAddress().getCity(),
                     pd.getAddress().getState(), pd.getMobileNumber(),
-                    (double) cd.getSalary(), DateUtils.asDate(cd.getJoining()),
+                    (double) cd.getSalary(), DateUtils.asDate(cd.getJoiningDate()),
                     cd.getPositionId());
             return rowsAffected;
         } catch (Exception e) {
--- src/main/java/com/jainva/api/services/EmployeServices.java
+++ src/main/java/com/jainva/api/services/EmployeServices.java
@@ -1,17 +1,13 @@
 package com.jainva.api.services;
 
-import com.jainva.api.dao.DataBaseQueries;
 import com.jainva.api.dao.EmployeeDaoImpl;
-import com.jainva.api.utils.DateUtils;
 import com.openapi.gen.springboot.dto.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.sql.Date;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
 
 @Component
 public class EmployeServices {
@@ -40,7 +36,7 @@ public class EmployeServices {
         p1.setName("Raghu Ram Rajan");
         p1.setMobileNumber("9238181930");
         c1.setSalary(1000000);
-        c1.setJoining(LocalDate.now());
+        c1.setJoiningDate(LocalDate.now());
         Address a1 = new Address();
         a1.city("Palo Alto");
         a1.country("USA");
@@ -59,7 +55,7 @@ public class EmployeServices {
         CorporateDetails c2 = new CorporateDetails();
         p2.setName("Mark Lohemyer");
         c2.setSalary(7000000);
-        c2.setJoining(LocalDate.now());
+        c2.setJoiningDate(LocalDate.now());
         Address a2 = new Address();
         a2.city("Palo Alto");
         a2.country("USA");
@@ -93,7 +89,12 @@ public class EmployeServices {
         return emp;
     }
 
-    public List<Employee> getAllEmployees() {
-        return new ArrayList<>();
+    public List<Employee> getAllEmployees() throws Exception{
+        try{
+            log.info("Getting all employees into DB");
+            return dbImpl.getAllEmployees();
+        }catch (Exception e){
+            throw new Exception("Failed to get all employee details due to" + e.getMessage());
+        }
     }
 }
